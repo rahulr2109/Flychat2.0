271cc3d396f7704813a728055e2adae72288f338
Rahul Rai <rahul0921r@gmail.com>
2024-02-19 23:33:35 +0530

    bug resolved

--- client/src/components/ScrollableChat.jsx
+++ client/src/components/ScrollableChat.jsx
@@ -13,7 +13,7 @@ const ScrollableChat = ({ messages }) => {
   const { user } = ChatState();
 
   return (
-    <ScrollableFeed bottom={0}>
+    <ScrollableFeed>
       {messages &&
         messages.map((m, i) => (
           <div style={{ display: "flex" }} key={m._id}>
--- client/src/components/SingleChat.jsx
+++ client/src/components/SingleChat.jsx
@@ -2,28 +2,16 @@ import { FormControl } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
 import "./styles.css";
-import {
-  ButtonGroup,
-  Flex,
-  Spacer,
-  IconButton,
-  Spinner,
-  useToast,
-  Heading,
-} from "@chakra-ui/react";
-import { getSender, getSenderFull } from "../config/ChatLogics";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { ArrowBackIcon } from "@chakra-ui/icons";
-import ProfileModal from "./miscellaneous/ProfileModal";
 import ScrollableChat from "./ScrollableChat";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
 import io from "socket.io-client";
-import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
 import { serverHost } from "../config/serverHost";
+import { Spinner, useToast } from "@chakra-ui/react";
 const ENDPOINT = serverHost;
 var socket, selectedChatCompare;
 
